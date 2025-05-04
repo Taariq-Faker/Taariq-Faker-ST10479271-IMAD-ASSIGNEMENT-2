be4d08f6845abd1e269155b1447107aae90b2086
ST10479271 <ST10479271@vcconnect.edu.za>
2025-05-04 15:18:28 +0200

    Starting Assignment 04/04/2025

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Taariq-Faker-ST10479271-IMAD-ASSIGNEMENT-2"
+include(":app")
+ 
