fc61ce5eb3fa4a46254e80c5dfc6432b002eb682
ST10479271 <ST10479271@vcconnect.edu.za>
2025-05-19 18:53:24 +0200

    -Added comments -Coded the third screen -Added a score button so that user can see there score again -Modified exit button so that is closes entire app -UI design changes 19/05/25

--- app/src/main/java/vcmsa/ci/taariq_faker_st10479271_imad_assignement_2/FlashcardQuestionScreen.kt
+++ app/src/main/java/vcmsa/ci/taariq_faker_st10479271_imad_assignement_2/FlashcardQuestionScreen.kt
@@ -7,9 +7,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.isVisible
 
 class FlashcardQuestionScreen : AppCompatActivity() {
 
@@ -24,7 +21,7 @@ class FlashcardQuestionScreen : AppCompatActivity() {
     )
 
 
-    //Deifning the array what will store the answers
+    //Defining the array what will store the answers
     var answers= booleanArrayOf(
         true,
         true,
@@ -33,16 +30,15 @@ class FlashcardQuestionScreen : AppCompatActivity() {
         true
     )
 
-      val userAnswer= BooleanArray(questions.size) {false
-      }
+    //Defining the array what will store the users answers.
+    //Setting the inital answers all to false
+      val userAnswer= BooleanArray(questions.size) {false }
 
-    //defining the score variable as well as setting it to 0 to ensure that it start at 0
+    //Defining the score variable as well as setting it to 0 to ensure that it start at 0
     var score=0
 
-    //defining the current index variable as well as setting it to 0 to ensure that it start at 0
+    //defining the current index variable as well as in order to keep track of which question is being displayed
     var currentIndex=0
-
-
 
 
 
@@ -52,25 +48,26 @@ class FlashcardQuestionScreen : AppCompatActivity() {
         setContentView(R.layout.activity_flashcard_question_screen)
 
 
-        //Defining the buttons and textview so that it is accessable in the program
+        //Defining the buttons and textview so that it is accessible in the program by referencing their ids form the layout
         val trueButton = findViewById<Button>(R.id.trueButton)
         val falseButton = findViewById<Button>(R.id.falseButton)
         val nextButton = findViewById<Button>(R.id.nextButton)
 
 
-
+        //Displaying the first question when the screen is showed
         showQuestion()
 
-
-            //Codinng the true button so then when pressed it performs as intended
+            //Coding the true button so then when pressed it performs as intended
         trueButton.setOnClickListener {
 
-            //When the button is clicked it cross referneces it the the check answer function
+            //When the button is clicked it cross references it the the check answer function
             checkAnswer(true)
         }
 
+        //Coding the false button so then when pressed it performs as intended
         falseButton.setOnClickListener {
 
+            //When the button is clicked it cross references it the the check answer function
             checkAnswer(false)
         }
 
@@ -78,10 +75,10 @@ class FlashcardQuestionScreen : AppCompatActivity() {
              //Coding the what happens once the next button is clicked
         nextButton.setOnClickListener {
 
+            //Used in order to move onto the next question
             currentIndex++
 
               //Using if statements to show the next question in the array until it reaches the end if the array
-
             if (currentIndex < questions.size) {
                 //Shows the question
                 showQuestion()
@@ -91,13 +88,17 @@ class FlashcardQuestionScreen : AppCompatActivity() {
                 // connecting to the score screen as well as carrying over the information collected for the various variables
                 val intent = Intent(this, ScoreScreen::class.java)
 
+                //Passing the score,total,questions,answers and user answers to the next screen
                 intent.putExtra("score", score)
                 intent.putExtra("total", questions.size)
                 intent.putExtra("Questions", questions)
                 intent.putExtra("Answer", answers)
                 intent.putExtra("userAnswer", userAnswer)
 
+                //stats the score screen activity
                 startActivity(intent)
+
+                //Finishes this activity so that the user is not able to continue with answering questions
                 finish()
 
 
@@ -106,17 +107,14 @@ class FlashcardQuestionScreen : AppCompatActivity() {
 
     }
 
-
-
-
-
-
+    //This function is used to display the current question
      private fun showQuestion (){
         val nextButton = findViewById<Button>(R.id.nextButton)
         val trueButton = findViewById<Button>(R.id.trueButton)
         val falseButton = findViewById<Button>(R.id.falseButton)
          val questionText=findViewById<TextView>(R.id.questionText)
 
+        //Sets the text to the question based on the current index
         questionText.text=questions[currentIndex]
 
 
@@ -128,7 +126,7 @@ class FlashcardQuestionScreen : AppCompatActivity() {
         nextButton.isEnabled=false
 
     }
-
+    //This function is used to check the users answers
     private fun checkAnswer(studentAnswer:Boolean)
     {
 
@@ -136,22 +134,29 @@ class FlashcardQuestionScreen : AppCompatActivity() {
         val falseButton = findViewById<Button>(R.id.falseButton)
         val nextButton = findViewById<Button>(R.id.nextButton)
 
+        //Records the users answers for the corresponding question
         userAnswer[currentIndex]=studentAnswer
 
-
-
+        //Using if statemnt to to compare the users answer to the correct answer of the question
         if (studentAnswer==answers[currentIndex]) {
+
+            //Displays a message to the user that they are correct
+            //Increments the score as well
             Toast.makeText(this@FlashcardQuestionScreen, "The answer is correct", Toast.LENGTH_LONG).show()
             score++
 
         }
         else {
+            //Displays a message to the user that they are incorrect
             Toast.makeText(this@FlashcardQuestionScreen, "The answer is incorrect", Toast.LENGTH_LONG).show()
 
         }
 
+        //Disables the True and False buttons after the user answers a question
         trueButton.isEnabled=false
         falseButton.isEnabled=false
+
+        //Enables the next button so that users can go onto the next question
         nextButton.isEnabled=true
 
 
--- app/src/main/java/vcmsa/ci/taariq_faker_st10479271_imad_assignement_2/ScoreScreen.kt
+++ app/src/main/java/vcmsa/ci/taariq_faker_st10479271_imad_assignement_2/ScoreScreen.kt
@@ -15,24 +15,26 @@ class ScoreScreen : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_score_screen)
 
+        //Retrieving the score,total,questions,answers and user answers to the flash card question screen
         val score=intent.getIntExtra("score",0)
         val total=intent.getIntExtra("total",0)
         val questions = intent.getStringArrayExtra("Questions")
         val answers=intent.getBooleanArrayExtra("Answer")
         val userAnswers=intent.getBooleanArrayExtra("userAnswer")
 
-
+        //Defining the buttons and textview so that it is accessible in the program by referencing their ids form the layout
         val reviewTextView=findViewById<TextView>(R.id.reviewTextView)
         val scoreText=findViewById<TextView>(R.id.scoreText)
         val feedbackTextView=findViewById<TextView>(R.id.feedbackText)
         val exitButton=findViewById<Button>(R.id.exitButton)
         val reviewButton=findViewById<Button>(R.id.reviewButton)
+        val scoreButton=findViewById<Button>(R.id.scoreButton)
 
 
-
+        //Displaying the users score
         scoreText.text="Score: $score out of $total"
 
-
+        //Using if statements to display feedback based on the users score
         if (score >=3 ){
             feedbackTextView.text="Great Job!"
         }
@@ -40,38 +42,59 @@ class ScoreScreen : AppCompatActivity() {
             feedbackTextView.text="Keep Practising!"
         }
 
+        //Programming the exit button
         exitButton.setOnClickListener {
-            finish()
+            //Closing the activity once the button is clicked
+            finishAffinity()
 
         }
 
+        //Programming the Review button
         reviewButton.setOnClickListener {
 
+            //Making sure all the arrays are not null before accessing them
             if ( questions!= null && answers!=null && userAnswers!=null){
 
+                //Creating a string builder to build the review text
                 val reviewText=StringBuilder()
+
+                //Uisng a loop to loop through all the questions and add the information to the review
                 for (i in questions.indices )
                 {
-                    reviewText.append("Q${i +1}: ${questions [i]} \n")
+                    reviewText.append(" ${questions [i]} \n")
                     reviewText.append("Correct Answer: ${answers[i]}\n")
                     reviewText.append("Your Answer: ${userAnswers[i]}\n \n ")
 
 
+                    //Clearing the score and feedback when showing the review
                     scoreText.text=""
                     feedbackTextView.text=""
 
                 }
 
+                //Displaying the review
                 reviewTextView.text=reviewText.toString()
                 reviewTextView.visibility= View.VISIBLE
 
             }
-
-
-
         }
 
+        //Coding the score Button
+        scoreButton.setOnClickListener {
 
+            //Clearing the review when showing the score and Feedback
+            reviewTextView.text=""
+
+            //Showing the score and feedback once again
+            scoreText.text="Score: $score out of $total"
+            if (score >=3 ){
+                feedbackTextView.text="Great Job!"
+            }
+            else{
+                feedbackTextView.text="Keep Practising!"
+            }
+
+        }
 
         }
     }
